56038c3e466424adabae4d3d21a0c31a12cc2de9
Mukta <muktadanu1995@gmail.com>
2021-02-02 14:56:23 +0530

    Autosuggestive dropdown

--- src/test/java/selenium/dropdowns/AutosuggestiveDropdown.java
+++ src/test/java/selenium/dropdowns/AutosuggestiveDropdown.java
@@ -1,5 +1,30 @@
 package selenium.dropdowns;
 
-public class AutosuggestiveDropdown {
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class AutosuggestiveDropdown extends BeforeTestExecution{
+
+	@Override
+	@Test
+	public void test() throws Exception {
+		driver.findElement(By.id("autosuggest")).sendKeys("New");
+		List<WebElement> options = driver.findElements(By.xpath("//ul[@id='ui-id-1']/li"));
+		Iterator<WebElement> it = options.iterator();
+		while(it.hasNext())
+		{
+			WebElement i = it.next();
+			String txt = i.getText();
+			if(txt.equals("Papua New Guinea"))
+			{
+				i.click();
+				break;
+			}
+		}
+	}
 
 }
