3eed7e19a033a8611655a94cdb126196d81890ca
Mukta <muktadanu1995@gmail.com>
2021-02-02 13:54:00 +0530

    Dynamic dropdown

--- src/test/java/selenium/dropdowns/DynamicDropdown.java
+++ src/test/java/selenium/dropdowns/DynamicDropdown.java
@@ -2,12 +2,17 @@ package selenium.dropdowns;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class DynamicDropdown extends BeforeTestExecution{
 
 	@Override
+	@Test
 	public void test() throws Exception {
-		WebElement origin = driver.findElement(By.id("ctl00_mainContent_ddl_originStation1"));
+		WebElement origin = driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT"));
+		origin.sendKeys("ja");
+		driver.findElement(By.xpath("//div[@id='dropdownGroup1']//div[@class='dropdownDiv']/ul/li/a[contains(text(),'Jaisalmer')]")).click();
+		Thread.sleep(3000);
 	}
 
 }
